4be3324ec479f4e86f305caf2337a54219c92d3e
Leonardo <leonardo@odeliverypro.com>
2023-09-04 13:05:45 -0300

    Sending an order updated message formated

--- src/OrderUpdatedMessage.php
+++ src/OrderUpdatedMessage.php
@@ -3,6 +3,9 @@
 namespace Peteleco\Notifier;
 
 use DateTimeInterface;
+use Peteleco\Notifier\Channels\Teams\Fact;
+use Peteleco\Notifier\Channels\Teams\MessageCard;
+use Peteleco\Notifier\Channels\Teams\Section;
 use Spatie\LaravelData\Data;
 
 class OrderUpdatedMessage extends Data
@@ -13,5 +16,32 @@ class OrderUpdatedMessage extends Data
         public readonly DateTimeInterface $updated_at
     )
     {
+    }
+
+    public function toMessageCard(): MessageCard
+    {
+        return MessageCard::from([
+            'summary' => 'Order updated!',
+            'sections' => [
+                Section::from([
+                    'activityTitle' => 'Order updated!',
+                    'activitySubtitle' => '',
+                    'facts' => [
+                        Fact::from([
+                            'name' => 'Order UUID',
+                            'value' => $this->uuid,
+                        ]),
+                        Fact::from([
+                            'name' => 'New status',
+                            'value' => $this->status,
+                        ]),
+                        Fact::from([
+                            'name' => 'Updated at',
+                            'value' => $this->updated_at,
+                        ]),
+                    ],
+                ]),
+            ],
+        ]);
     }
 }
--- tests/NotifierTest.php
+++ tests/NotifierTest.php
@@ -1,11 +1,57 @@
 <?php
 
 use Illuminate\Http\Response;
+use Peteleco\Notifier\Channels\Teams\Fact;
+use Peteleco\Notifier\Channels\Teams\MessageCard;
+use Peteleco\Notifier\Channels\Teams\Section;
+use Peteleco\Notifier\Notifier;
+use Peteleco\Notifier\OrderUpdatedMessage;
 
 it('send a basic message', function () {
-    $message = app(
-        \Peteleco\Notifier\Notifier::class,
+    $request = app(
+        Notifier::class,
         ['webhookUrl' => config('notifier.hooks.orders.updated')]
     )->sendMessage('Hello world!');
-    expect($message->status())->toBe(Response::HTTP_OK);
+    expect($request->status())->toBe(Response::HTTP_OK);
+});
+
+it('send a complex message', function () {
+    $message = MessageCard::from([
+        'summary' => 'Order updated!',
+        'sections' => [
+            Section::from([
+                'activityTitle' => 'Order updated!',
+                'activitySubtitle' => '',
+                'facts' => [
+                    Fact::from([
+                        'name' => 'First Fact',
+                        'value' => 'Value of First Fact',
+                    ]),
+                    Fact::from([
+                        'name' => 'Second Fact',
+                        'value' => 'Value of Second Fact',
+                    ]),
+                ],
+            ]),
+        ],
+    ]);
+
+    $request = app(
+        Notifier::class,
+        ['webhookUrl' => config('notifier.hooks.orders.updated')]
+    )->send($message);
+    expect($request->status())->toBe(Response::HTTP_OK);
+});
+
+it('send an order updated message', function () {
+    $message = OrderUpdatedMessage::from([
+        'uuid' => \Illuminate\Support\Str::uuid(),
+        'status' => 'my custom status',
+        'updated_at' => \Carbon\Carbon::yesterday()->setHour(18)->setMinutes(30)->setSecond(0)
+    ]);
+    $request = app(
+        Notifier::class,
+        ['webhookUrl' => config('notifier.hooks.orders.updated')]
+    )->send($message->toMessageCard());
+    expect($request->status())->toBe(Response::HTTP_OK);
 });
